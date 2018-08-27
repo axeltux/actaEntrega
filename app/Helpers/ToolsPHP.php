81c68e517cae7d54114665d9e65429e24a0d8509
unknown <axeltux@gmail.com>
2018-08-27 12:11:58 -0500

    cambios proyecto

--- app/Helpers/ToolsPHP.php
+++ app/Helpers/ToolsPHP.php
@@ -9,19 +9,17 @@ class ToolsPHP {
 	 * Retorna el nombre de un empleado
 	*/
     public static function NomEmpleados($id){
-    	$dato = DB::table('vs_empleados_p_DR')->select('*')
-								            ->where('NumeroEmpleado','=',$id)
-								            ->get();
-		if(count($dato)>=1){
-			return $dato[0]->Nombre . " " . $dato[0]->APaterno . " " . $dato[0]->AMaterno;
-		}else{
-			return "- - -";
-		}
-	}
+    	$dato = DB::table('vs_empleados_p_DR')->select('*')->where('NumeroEmpleado','=',$id)->get();
+            if(count($dato)>=1){
+                    return $dato[0]->Nombre . " " . $dato[0]->APaterno . " " . $dato[0]->AMaterno;
+            }else{
+                    return "- - -";
+            }
+    }
 
-	public static function NombreMes($mes){
-		setlocale(LC_TIME, 'spanish');
-		$nombre=strftime("%B",mktime(0, 0, 0, $mes, 1, 2000));
-		return $nombre;
-	}
+    public static function NombreMes($mes){
+            setlocale(LC_TIME, 'spanish');
+            $nombre=strftime("%B",mktime(0, 0, 0, $mes, 1, 2000));
+            return $nombre;
+    }
 }
