185f150acb7d4aa7ae44752ecb2e7c3fa274b2e8
axeltux <axeltux@gmail.com>
2018-12-05 16:28:12 -0600

    Ajustes en vista users.editar y eliminacion de archivos temporales de la carpeta .phpintel

--- resources/views/users/editar.blade.php
+++ resources/views/users/editar.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -88,4 +89,5 @@
         </div>
     </div>
 </div>
+
 @endsection
